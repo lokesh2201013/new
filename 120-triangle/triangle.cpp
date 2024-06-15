0746bfa2f35863a80745e2be39401a5e7409727a
lokesh2201013 <135566992+lokesh2201013@users.noreply.github.com>
2024-06-15 21:28:31 +0530

    Time: 4 ms (73.15%) | Memory: 11.4 MB (32.51%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,19 +1,23 @@
 class Solution {
-private:
-    int give(vector<vector<int>>& t, vector<vector<int>>& dp, int i, int j) {
-        if (i == t.size() - 1) return t[i][j];
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int d = t[i][j] + give(t, dp, i + 1, j);
-        int dg = t[i][j] + give(t, dp, i + 1, j + 1);
-
-        return dp[i][j] = min(d, dg);
-    }
-
 public:
     int minimumTotal(vector<vector<int>>& t) {
         if (t.empty()) return 0;
-        vector<vector<int>> dp(t.size(), vector<int>(t.size(), -1));
-        return give(t, dp, 0, 0);
+
+        int n = t.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        // Initialize the last row of dp with the last row of triangle
+        for (int j = 0; j < n; ++j) {
+            dp[n-1][j] = t[n-1][j];
+        }
+
+        // Fill dp array from bottom to top
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = 0; j <= i; ++j) {
+                dp[i][j] = t[i][j] + min(dp[i + 1][j], dp[i + 1][j + 1]);
+            }
+        }
+
+        return dp[0][0];
     }
 };
