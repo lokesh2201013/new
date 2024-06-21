6bbe0256f99c42b54b9ed21f0a24d312684e9072
lokesh2201013 <135566992+lokesh2201013@users.noreply.github.com>
2024-06-22 05:19:13 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (73.04%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    bool search(vector<int>& n, int t) {
-        int l = 0, r = n.size() - 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            while (l < mid && n[l] == n[mid]) l++;
-            while (r > mid && n[r] == n[mid]) r--;
+    int search(vector<int>& n, int t) {
+        int l=0,r=n.size()-1;
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
             
-            if (n[mid] == t || n[l] == t || n[r] == t) return true;
-
-            if (n[l] <= n[mid]) {
-                if (n[l] <= t && t < n[mid]) {
-                    r = mid - 1;
-                } else {
-                    l = mid + 1;
-                }
-            } else {
-       
-                if (n[mid] < t && t <= n[r]) {
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
-                }
+            if(n[mid]==t||n[l]==t||n[r]==t)return true;
+            if(n[l]==n[mid]&&n[mid]==n[r])
+            {
+                l++;r--;continue;
+            }
+            if(n[l]<=n[mid])
+            {
+                if(n[l]<=t&&t<=n[mid])r=mid-1;
+                else l=mid+1;
+            }
+            else
+            {
+                if(n[mid]<=t&&t<=n[r])l=mid+1;
+                else r=mid-1;
             }
         }
         return false;
