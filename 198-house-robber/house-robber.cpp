b291bbd4c7d04186738efd451994da0c16a855f6
lokesh2201013 <135566992+lokesh2201013@users.noreply.github.com>
2024-06-30 03:20:58 +0530

    Time: 4 ms (31.47%) | Memory: 9.2 MB (59.98%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    int give(int i, vector<int>& n, vector<int>& dp) {
-        if (i < 0) return 0;
-        if (dp[i] != -1) return dp[i];
-        int rob = n[i] + give(i - 2, n, dp);
-        int notRob = give(i - 1, n, dp);
-        return dp[i] = max(rob, notRob);
-    }
-
     int rob(vector<int>& n) {
-        vector<int> dp(n.size(), -1);
-        return give(n.size() - 1, n, dp);
+    if(n.empty()) return 0;
+    int s=n.size();
+      vector<int> dp(s, 0);
+    if(s==1) return n[0];
+    dp[0]=n[0];
+    dp[1]=max(n[0],n[1]);
+    for(int i=2 ; i<s ; i++)
+    {
+        dp[i]=max(dp[i-1],dp[i-2]+n[i]);
+    }
+    return dp[s-1];
     }
 };
