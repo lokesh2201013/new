ed8ae5f91154892e2d37d3baaccdd18ccd724cf6
lokesh2201013 <135566992+lokesh2201013@users.noreply.github.com>
2024-07-12 01:05:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (17.58%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,24 +1,21 @@
 class Solution {
-private:
-    int give(int i, int j, std::vector<std::vector<int>>& dp) {
-        if (i == 0 && j == 0) {
-            return 1;
-        }
-        if (i < 0 || j < 0) {
-            return 0;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        int up = give(i - 1, j, dp);
-        int left = give(i, j - 1, dp);
+public:
+    int m, n;
+    int give(vector<vector<int>>& dp, int i, int j) {
+        if (i < 0 || j < 0 || i >= m || j >= n) return 0;
+        if (i == m - 1 && j == n - 1) return 1;
+        if (dp[i][j] != -1) return dp[i][j];
         
-        return dp[i][j] = up + left;
+        int d = give(dp, i + 1, j);
+        int r = give(dp, i, j + 1);
+        
+        return dp[i][j] = d + r;
     }
 
-public:
     int uniquePaths(int m, int n) {
-        std::vector<std::vector<int>> dp(m, std::vector<int>(n, -1));
-        return give(m - 1, n - 1, dp);
+        this->m = m;
+        this->n = n;
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return give(dp, 0, 0);
     }
 };
