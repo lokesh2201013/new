8f16a75ea7ec351dbb574a83fe3cc47f5657b0ec
lokesh2201013 <135566992+lokesh2201013@users.noreply.github.com>
2024-10-01 15:40:41 +0530

    Time: 4 ms (32.23%) | Memory: 9.6 MB (47.08%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int give(vector<int>& n, vector<int>& dp, int i) {
-        if (i < 0) return 0;
-        if (i == 0) return n[0];
-        if (dp[i] != -1) return dp[i];
-        
-        int robCurrent = n[i] + give(n, dp, i - 2);
-        int skipCurrent = give(n, dp, i - 1);
-        
-        return dp[i] = max(robCurrent, skipCurrent);
+        if (i >= n.size()) {
+            return 0;
+        }
+
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        int l = give(n, dp, i + 1); 
+        int r = give(n, dp, i + 2) + n[i];
+
+        return dp[i] = max(l, r);
     }
 
     int rob(vector<int>& n) {
-        if (n.size() == 0) return 0;
-        if (n.size() == 1) return n[0];
-        
         vector<int> dp(n.size(), -1);
-        return give(n, dp, n.size() - 1);
+        return give(n, dp, 0);
     }
 };
