8f23832ccf98d09a597da5557751191954fcce50
lokesh2201013 <135566992+lokesh2201013@users.noreply.github.com>
2024-12-18 08:36:47 +0530

    Time: 3 ms (44.14%) | Memory: 15.3 MB (38.54%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -3,7 +3,7 @@ public:
     void groupAndPush(vector<tuple<int, int, int>>& a, vector<vector<int>>& af) {
         map<int, vector<int>> grouped;
         for (const auto& t : a) {
-            int x = get<1>(t); // Extract vertical order (x)
+            int x = get<1>(t);
             int val = get<0>(t); 
             grouped[x].push_back(val);
         }
@@ -20,14 +20,14 @@ public:
     }
 
     vector<vector<int>> verticalTraversal(TreeNode* r) {
-        vector<tuple<int, int, int>> a; // Stores {value, x (vertical), y (horizontal)}
+        vector<tuple<int, int, int>> a; 
         traverse(r, a, 0, 0);
 
-        // Sort by: vertical (x), then horizontal (y), then node value
+       
         sort(a.begin(), a.end(), [](const tuple<int, int, int>& t1, const tuple<int, int, int>& t2) {
-            if (get<1>(t1) != get<1>(t2)) return get<1>(t1) < get<1>(t2); // Compare x
-            if (get<2>(t1) != get<2>(t2)) return get<2>(t1) < get<2>(t2); // Compare y
-            return get<0>(t1) < get<0>(t2); // Compare value
+            if (get<1>(t1) != get<1>(t2)) return get<1>(t1) < get<1>(t2); 
+            if (get<2>(t1) != get<2>(t2)) return get<2>(t1) < get<2>(t2); 
+            return get<0>(t1) < get<0>(t2);
         });
 
         vector<vector<int>> af;
