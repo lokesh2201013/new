806eba01e058ca576063d84b6bc409bdf09aaf6d
lokesh2201013 <135566992+lokesh2201013@users.noreply.github.com>
2024-12-24 01:56:47 +0530

    Time: 94 ms (58.36%) | Memory: 198.8 MB (44.28%) - LeetSync

--- 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
@@ -6,13 +6,13 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x, left(left), right(right)) {}
  * };
  */
 class Solution {
 public:
     int minimumOperations(TreeNode* r) {
-        vector<vector<int>> s;
+        vector<vector<int>> levels;
         queue<pair<TreeNode*, int>> q;
         q.push({r, 0});
         
@@ -20,32 +20,47 @@ public:
             TreeNode* node = q.front().first;
             int lvl = q.front().second;
             q.pop();
-            
-            if (s.size() <= lvl) {
-                s.push_back({});
+
+            if (levels.size() <= lvl) {
+                levels.push_back({});
             }
-            
-            s[lvl].push_back(node->val);
-            
+            levels[lvl].push_back(node->val);
+
             if (node->left != nullptr) q.push({node->left, lvl + 1});
             if (node->right != nullptr) q.push({node->right, lvl + 1});
         }
-        
-        int c = 0;
-        for (int i = 0; i < s.size(); i++) {
-            for (int j = 0; j < s[i].size(); j++) {
-                int m = *min_element(s[i].begin() + j, s[i].end());
-                if (s[i][j] == m) continue;
-                else {
-                    auto it = find(s[i].begin() + j, s[i].end(), m);
-                    if (it != s[i].end()) {
-                        int fo = distance(s[i].begin(), it);
-                        swap(s[i][j], s[i][fo]);
-                        c++;
-                    }
+
+        int swaps = 0;
+
+        for (auto& level : levels) {
+            vector<pair<int, int>> valIndexPairs;
+
+            for (int i = 0; i < level.size(); i++) {
+                valIndexPairs.push_back({level[i], i});
+            }
+
+            sort(valIndexPairs.begin(), valIndexPairs.end());
+
+           
+            vector<bool> visited(level.size(), false);
+            for (int i = 0; i < valIndexPairs.size(); i++) {
+                if (visited[i] || valIndexPairs[i].second == i) {
+                    continue;
+                }
+                int cycleSize = 0;
+                int j = i;
+
+                while (!visited[j]) {
+                    visited[j] = true;
+                    j = valIndexPairs[j].second;
+                    cycleSize++;
+                }
+                if (cycleSize > 1) {
+                    swaps += (cycleSize - 1);
                 }
             }
         }
-        return c;
+
+        return swaps;
     }
 };
