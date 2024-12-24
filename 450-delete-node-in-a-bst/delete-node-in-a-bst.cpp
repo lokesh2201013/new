586b201cc5016139319aa49cf06c144d062cf395
lokesh2201013 <135566992+lokesh2201013@users.noreply.github.com>
2024-12-24 18:13:33 +0530

    Time: 0 ms (100.00%) | Memory: 34.3 MB (11.47%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    TreeNode* deleteNode(TreeNode* root, int key) {
-        vector<int> inorder;
+    TreeNode* deleteNode(TreeNode* r, int k) {
+        if (!r) return nullptr; 
 
-        inorderTraversal(root, inorder);
-  
-        auto it = find(inorder.begin(), inorder.end(), key);
-        if (it != inorder.end()) {
-            inorder.erase(it);
-        }
-        
-        
-        return buildTree(inorder, 0, inorder.size() - 1);
-    }
-
-private:
     
-    void inorderTraversal(TreeNode* root, vector<int>& inorder) {
-        if (root == nullptr) return;
-        inorderTraversal(root->left, inorder);
-        inorder.push_back(root->val);
-        inorderTraversal(root->right, inorder);
-    }
-
-
-    TreeNode* buildTree(const vector<int>& inorder, int left, int right) {
-        if (left > right) return nullptr;
-        
-        int mid = left + (right - left) / 2;
-        TreeNode* root = new TreeNode(inorder[mid]);
-        
-        root->left = buildTree(inorder, left, mid - 1);
-        root->right = buildTree(inorder, mid + 1, right);
-        
-        return root;
+        if (r->val > k) {
+            r->left = deleteNode(r->left, k);
+        } else if (r->val < k) {
+            r->right = deleteNode(r->right, k);
+        } else { 
+            if (!r->left) {
+                TreeNode* temp = r->right;
+                delete r;
+                return temp;
+            } else if (!r->right) {
+                TreeNode* temp = r->left;
+                delete r;
+                return temp;
+            } else { 
+                TreeNode* successor = r->right;
+                while (successor->left) {
+                    successor = successor->left;
+                }
+                r->val = successor->val;
+                r->right = deleteNode(r->right, successor->val);
+            }
+        }
+        return r;
     }
 };
