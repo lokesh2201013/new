dd92ae817ebcf80dd317b39c66a2d6c2ea024494
lokesh2201013 <135566992+lokesh2201013@users.noreply.github.com>
2024-12-28 00:18:05 +0530

    Time: 4 ms (80.92%) | Memory: 128.2 MB (62.06%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& n) {
-        vector<int> o;
-        vector<int> e;
-
-        for (int i = 0; i < n.size(); i++) {
-            if (n[i] < 0) {
-                o.push_back(n[i]);
-            } else {
-                e.push_back(n[i]);
-            }
-        }
-
-        n.clear();
-
-        int size = min(e.size(), o.size());  // Use the size of the smaller vector
-        for (int i = 0; i < size; i++) {
-            n.push_back(e[i]);
-            n.push_back(o[i]);
-        }
-
-        return n;
-    }
+     vector<int>a(n.size(),0);
+     int p=0,ne=1;
+     for(int i=0 ; i<n.size();i++){
+       if(n[i]>=0){
+        a[p]=n[i];
+        p+=2;
+       }else{
+        a[ne]=n[i];
+        ne+=2;
+       }
+     }
+     return a;
+     }   
 };
