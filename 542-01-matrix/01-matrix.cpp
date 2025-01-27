65302ac08fcc6b1eead0c823338752a46064c255
lokesh2201013 <135566992+lokesh2201013@users.noreply.github.com>
2025-01-27 06:55:55 +0530

    Time: 24 ms (41.61%) | Memory: 39 MB (24.49%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        vector<vector<int>> dis(n, vector<int>(m, 0));
-        queue<pair<pair<int, int>, int>> q;
-    
-        int delr[] = {-1, 1, 0, 0};
-        int delc[] = {0, 0, -1, 1};
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 0) {
+    vector<vector<int>> updateMatrix(vector<vector<int>>& m) {
+        vector<vector<int>> v(m.size(), vector<int>(m[0].size(), 0));
+        vector<vector<int>> d(m.size(), vector<int>(m[0].size(), 0));
+        queue<pair<pair<int, int>, int>> q; 
+
+        for (int i = 0; i < m.size(); i++) {  
+            for (int j = 0; j < m[0].size(); j++) {
+                if (m[i][j] == 0) { 
                     q.push({{i, j}, 0});
-                    vis[i][j] = 1;
-                } else {
-                    vis[i][j] = 0;
+                    v[i][j] = 1; 
                 }
             }
         }
-        
+        vector<int> dr = {-1, 0, 1, 0}; 
+        vector<int> dc = {0, 1, 0, -1}; 
         while (!q.empty()) {
             int r = q.front().first.first;
             int c = q.front().first.second;
             int s = q.front().second;
             q.pop();
-            dis[r][c] = s;
+            d[r][c] = s;
+
             for (int i = 0; i < 4; i++) {
-                int nr = r + delr[i];
-                int nc = c + delc[i];
-                if (nr >= 0 && nc >= 0 && nr < n && nc < m && vis[nr][nc] == 0) {
-                    vis[nr][nc] = 1;
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if (nr >= 0 && nr < m.size() && nc >= 0 && nc < m[0].size() && v[nr][nc] != 1) {
                     q.push({{nr, nc}, s + 1});
+                    v[nr][nc] = 1; 
                 }
             }
         }
-        return dis;
+        return d;
     }
 };
